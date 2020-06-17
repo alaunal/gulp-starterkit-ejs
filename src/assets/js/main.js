d0e2606e14ddcefaf0eaa4cee4288ba34744f470
alaunal kauniyyah <alaunalkauniyyah3@gmail.com>
2020-06-17 22:51:37 +0700

    create page of 404

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,3 +1,18 @@
+const page404 = () => {
+    let container = document.querySelector('.c-404__wrapper');
+
+    window.onmouseover = (e) => {
+        let x = -e.clientX / 5,
+            y = -e.clientY / 5;
+
+        container.style.backgroundPositionX = x + 'px';
+        container.style.backgroundPositionY = y + 'px';
+    };
+};
+
+
+page404();
+
 window.addEventListener('load', () => {
     console.log('main js is loaded');
 });
